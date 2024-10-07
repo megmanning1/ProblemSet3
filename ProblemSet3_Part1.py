98b5ca403b3decd213f9e720140958e33bd5450e
megmanning1 <megmanning1@gmail.com>
2024-10-07 12:30:44 -0400

    Task 1 complete

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,19 @@
+#-------------------------------------------------------------
+# ProblemSet3_Part1.py
+#
+# Author: Meg Manning (meg.manning@duke.edu)
+# Date:   October 2024
+#
+#--------------------------------------------------------------
 #%% Task 1 - Edit code to print as requested
 /*-PS3: Code Block 1--*/
 
 mountain = "Denali"
 nickname = 'Mt. McKinley'
-elevation == 20322 
+elevation = 20322 
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+print (f"{mountain}, sometimes \ncalled \"{nickname}\",")
+#print (f"called \"{nickname}\",")
+print (f"is {elevation}' above sea level.")
 
 #%% Task 2 - Lists and Iteration
