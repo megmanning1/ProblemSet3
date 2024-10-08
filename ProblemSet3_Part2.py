40f10b66bb920f2f08987955dc4fbd06ad7b18ab
megmanning1 <megmanning1@gmail.com>
2024-10-08 14:33:14 -0400

    Task 5 (and bonus) complete

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -64,3 +64,57 @@ print(vesselDict[vesselID])
 
 #Print statement
 print(f"Vessel # {vesselID} flies the flag of {vesselDict[vesselID]}")
+
+#%% Task 5 
+
+#Open Loitering Dataset
+LoiteringObj = open(file='data/raw/loitering_events_20180723.csv',mode='r')
+
+#Read the entire contents into a list object
+line_list = LoiteringObj.readlines()
+
+#Release the link to the file objects (now that we have all its contents)
+LoiteringObj.close() #Close the file
+
+#Create string of header items
+header = line_list[0]
+header_string = header.split(",")
+print(header_string)
+
+#Create index values for each parameters
+transshipment_mmsi_idx = header_string.index("transshipment_mmsi")
+starting_lat_idx = header_string.index("starting_latitude")
+ending_lat_idx = header_string.index("ending_latitude")
+starting_lon_idx = header_string.index("starting_longitude")
+ending_lon_idx = header_string.index("ending_longitude")
+
+print(transshipment_mmsi_idx,starting_lat_idx,ending_lat_idx,starting_lon_idx,ending_lon_idx)
+
+#Loop through each line of data (skipping header)
+for eachline in line_list[1:]:
+    #Split the data into values
+    line_data = eachline.split(",")
+   
+    #Store parameters into own variables 
+    #Had issues with data types so converted parameters to floats
+    trans_mmsi = line_data[transshipment_mmsi_idx]
+    starting_lat = float(line_data[starting_lat_idx])
+    ending_lat = float(line_data[ending_lat_idx])
+    starting_lon = float(line_data[starting_lon_idx])
+    ending_lon = float(line_data[ending_lon_idx])
+
+    #examine start and end lat & create boolean variable if it crosses equator
+    crosses_equator = starting_lat < 0 < ending_lat
+
+    #examine start and end lon & create boolean variable if falls in range
+    #within_lon_range = starting_lon > 145 and ending_lon < 155
+            #tried above originally, but was getting an extra Kiribati vessel so altered to below
+    within_lon_range = 145 < starting_lon < 155
+
+    #if both variables are true, query dictionary
+    if crosses_equator and within_lon_range:
+        fleet_name = vesselDict.get(trans_mmsi)
+        if fleet_name:
+            print(f"Vessel #{trans_mmsi} {starting_lon} flies the flag of {fleet_name}")
+        else:
+            print(f"No vessels met criteria")
