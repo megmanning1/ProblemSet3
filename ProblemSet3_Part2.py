1c62839b8fec4e38e715fbfa9d10cea970047104
megmanning1 <megmanning1@gmail.com>
2024-10-08 10:13:45 -0400

    Task 4.1 complete

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -5,4 +5,21 @@
 # Date:   October 2024
 #
 #--------------------------------------------------------------
-#%% 
+#%% Task 4.1 
+
+#Create a Python file object, i.e., a link to the file's contents, mode = 'r' to read contents
+fileObj = open(file='data/raw/transshipment_vessels_20180723.csv',mode='r')
+
+#Read the entire contents into a list object
+lineList = fileObj.readlines()
+
+#Release the link to the file objects (now that we have all its contents)
+fileObj.close() #Close the file
+
+#Save the contents of the first line in the list of lines to the variable "headerLineString"
+headerLineString = lineList[0]
+
+#Print the contents of the headerLine
+print(headerLineString)
+
+# %%
